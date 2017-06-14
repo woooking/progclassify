79f75e52ed79e4288e56594073604377725863bf
byw <byw@pku.edu.cn>
2017-06-14 13:40:52 +0800

    save weights

--- cnn/cnnclassifier.py
+++ cnn/cnnclassifier.py
@@ -60,35 +60,53 @@ learning_rate = 0.001
 optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)
 correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(network, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+saver = tf.train.Saver()
 
 # Training
 #trainop = TrainOp(loss=loss, optimizer=optimizer, metric=accuracy)
 #model = Trainer(train_ops=trainop, tensorboard_verbose=0)
-with tf.Session() as sess:
-	tf.global_variables_initializer().run()
-	for _ in range(10001):
-		trainX, trainY = batch_generator(64, True)
-		res = sess.run([optimizer, loss], feed_dict={X: trainX, Y: trainY})
-		#val = model.fit_batch(feed_dicts={X: trainX, Y: trainY})
-		if _ % 10 == 0:
-			print("loss: {}".format(res[1]))
-		if _ % 100 == 0:
-			testX, testY = batch_generator(200, False)
-			print("accuracy: {}".format(accuracy.eval(feed_dict={X: testX, Y: testY})))
-		if _ % 1000 == 0 and _ != 0:
-			result = 0.0
-			num = 0
-			for d, l in generator.test_cases():
-				testX, testY = [], []
-				data = np.array(d[0])
-				if len(data) < maxL:
-					data = np.lib.pad(data, ((0, maxL - len(data)), (0, 0)), 'constant')
-				else:
-					data = data[:maxL]
-				testX.append(data)
-				testY.extend(l)
-				result += accuracy.eval(feed_dict={X: testX, Y: testY})
-				num += 1
-				if num % 100 == 0:
-					print(num, result / num)
-			print("test accuracy: {}".format(result / num))
+def test(restore=False, sess=None):
+	if restore:
+		sess = tf.Session()
+		saver.restore(sess, "../model/word_embedding_cnn")
+	result = 0.0
+	num = 0
+	for d, l in generator.test_cases():
+		testX, testY = [], []
+		data = np.array(d[0])
+		if len(data) < maxL:
+			data = np.lib.pad(data, ((0, maxL - len(data)), (0, 0)), 'constant')
+		else:
+			data = data[:maxL]
+		testX.append(data)
+		testY.extend(l)
+		result += accuracy.eval(session=sess, feed_dict={X: testX, Y: testY})
+		num += 1
+		if num % 100 == 0:
+			print(num, result / num)
+	print("test accuracy: {}".format(result / num))
+	return result / num
+
+
+def train():
+	with tf.Session() as sess:
+		tf.global_variables_initializer().run()
+		maxAcc = 0.97
+		for _ in range(20001):
+			trainX, trainY = batch_generator(64, True)
+			res = sess.run([optimizer, loss], feed_dict={X: trainX, Y: trainY})
+			#val = model.fit_batch(feed_dicts={X: trainX, Y: trainY})
+			if _ % 10 == 0:
+				print("loss: {}".format(res[1]))
+			if _ % 100 == 0:
+				testX, testY = batch_generator(200, False)
+				print("accuracy: {}".format(accuracy.eval(feed_dict={X: testX, Y: testY})))
+			if _ % 1000 == 0 and _ != 0:
+				acc = test(sess=sess)
+				if acc > maxAcc:
+					maxAcc = acc
+					saver.save(sess, "../model/word_embedding_cnn")
+
+if __name__ == '__main__':
+	train()
+	#test(restore=True)
